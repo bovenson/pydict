4efcfa40f42ddf0b957094e35fb4fc5af0fa65d3
bovenson <szhkai@126.com>
2016-01-17 14:16:33 +0800

    1.pydict-gui.py version 1.0.0

--- spider/pydict/pydict-gui/pydict-gui.py
+++ spider/pydict/pydict-gui/pydict-gui.py
@@ -1,34 +1,74 @@
 #!/usr/bin/python3
 # coding:utf-8
 from tkinter_funs_bovenson import *
-#import turtle
+from translate import *
 import tkinter as tk
 
 class App(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
-        self.pack()
+        self.init()
         self.createWidgets()
+        self.grid()
+
+        self.cls_trans = Translate()
+        pass
+    def init(self):
+        #属性
+        self.screen_width = root.maxsize()[0]
+        self.screen_height = root.maxsize()[1]
+        self.app_width = int(self.screen_width*2/3)
+        self.app_height = int(self.screen_height*2/3)
+
+        #设置窗口属性
+        self.master.title("pydict")
+        self.master.resizable(False, False)
+
+        #设置窗口大小
+        #self.master.geometry('%dx%d' % (self.app_width, self.app_height))
+        #center_window(self.master, self.app_width, self.app_height)
+
+        #添加组件
+        self.label_origin_tip = tk.Label(root, text='输入要翻译的文本:')
+        self.label_origin_tip.grid(row=0, column=0, sticky=tk.W)
+
+        self.label_transtype_text = tk.StringVar()
+        self.label_transtype = tk.Label(root, textvariable=self.label_transtype_text)
+        self.label_transtype.grid(row=0, column=1, sticky=tk.W)
+
+        self.label_trans_tip = tk.Label(root, text=':翻译结果')
+        self.label_trans_tip.grid(row=0, column=1, sticky='E')
+        
+        self.text_origin = tk.Text(root)
+        self.text_origin.grid(row=1, column=0)
+        self.text_trans = tk.Text(root)
+        self.text_trans.grid(row=1, column=1)
+
+        self.button_ok2trans = tk.Button(root, text='翻译', command=self.translate)
+        self.button_ok2trans.grid(row=2, columnspan=2)
+
+        pass
+    def update_text_trans(self, text):
+        self.text_trans.state=tk.NORMAL
+        self.text_trans.delete(0.0, tk.END)
+        self.text_trans.insert(tk.INSERT, text)
+        self.text_trans.state=tk.DISABLED
+        pass
+    def translate(self):
+        content = self.text_origin.get(0.0, tk.END)
+        res = self.cls_trans.translate(content)
+        trans = res.trans
+        self.update_text_trans(trans)
+        self.label_transtype_text.set('翻译类型: ' + res.lang1 + ' --> ' + res.lang2 + '    来源:' + res.trans_source)
+        pass
     def createWidgets(self):
+        pass
+    def __del__(self):
+        self.cls_trans.load_offline_data()
         pass
 
 #创建应用
 root = tk.Tk()
 app = App(master=root)
-
-#属性
-screen_width = root.maxsize()[0]
-screen_height = root.maxsize()[1]
-app_width = int(screen_width*2/3)
-app_height = int(screen_height*2/3)
-
-#设置窗口属性
-app.master.title("pydict")
-app.master.resizable(False, False)
-#设置窗口大小
-app.master.geometry('%dx%d' % (app_width, app_height))
-center_window(root, app_width, app_height)
-#app.master.maxsize(app_width, app_height)
-
 #运行窗口程序
 app.mainloop()
---
+++ spider/pydict/pydict-gui/translate.py
@@ -0,0 +1,111 @@
+import urllib.request
+import urllib.parse
+import json
+import pickle
+import os.path
+# 有道词典请求头部信息
+head = {}
+head['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safari/537.36 SE 2.X MetaSr 1.0'
+# 有道词典POST的数据内容
+url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule&smartresult=ugc&sessionFrom=http://www.youdao.com/'
+data = {}
+data['type'] = 'auto'
+data['i'] = ""
+data['doctype'] = 'json'
+data['xmlVersion'] = '1.8'
+data['keyfrom'] = 'fanyi.web'
+data['ue'] = 'UTF-8'
+data['action'] = 'FY_BY_CLICKBUTTON'
+data['typoResult'] = 'true'
+
+language_map = {'ZH_CN':'中文', 'EN':'英语', 'RU':'俄语', 'JA':'日语', 'SP':'西班牙语', 'KR':'韩语'}
+
+def formart_content(content):
+    '''
+    格式化要查询的文本
+    去除开头结尾空字符;连续的空字符缩为一个空符
+    '''
+    return  ' '.join(filter(lambda x:x, content.split()))
+
+class DataStructure:
+    def __init__(self, lang1='none', lang2='none', trans='无法完成操作,是否无法连接网络?', trans_source='none'):
+        self.lang1 = lang1
+        self.lang2 = lang2
+        self.trans = trans
+        self.trans_source = trans_source
+        pass
+
+class Translate:
+    '''
+    翻译模块
+    '''
+    def __init__(self):
+        self.count= 0
+        self.trans_source = ''
+        self.offline_data = {}
+        self.save_data_limit_count = 10
+        self.load_offline_data()
+        pass
+    def translate(self, content):
+        '''翻译'''
+        content = formart_content(content)
+        if content not in self.offline_data:
+            self.count += 1
+            if (self.count >= self.save_data_limit_count):
+                self.save_offline_data_to_file()
+                self.count = 0
+                pass
+            pass
+        #在线查询
+        res = self.online_trans(content)
+        if res == None:
+            if content in self.offline_data.keys():
+                res = self.offline_data[content]
+                res.trans_source = '离线查询结果'
+            else:
+                res = DataStructure()
+        if res == None:
+            res = DataStructure()
+        return res
+    def load_offline_data(self):
+        '''
+        从本地加载查询结果
+        '''
+        if os.path.exists('offline_data'):
+            print('正在从本地加载查询结果...')
+            try:
+                with open('offline_data', 'rb') as pickle_file:
+                    self.offline_data = pickle.load(pickle_file)
+                print('从本地加载', len(self.offline_data), '条数据')
+            except BaseException as reason:
+                print('读取离线文件时遇到未知错误:',reason, '\n')
+        pass
+    def save_offline_data_to_file(self):
+        '''
+        将查询结果保存到本地
+        '''
+        print('正在保存查询结果到本地...')
+        try:
+            with open('offline_data', 'wb') as pickle_file:
+                pickle.dump(self.offline_data, pickle_file)
+        except BaseException as reason:
+            print('保存离线文件时遇到未知错误:', reason)
+        pass
+    def online_trans(self, content):
+        '''在线查询'''
+        try:
+            data['i'] = content
+            urldata = urllib.parse.urlencode(data).encode('utf-8')
+            req = urllib.request.Request(url, urldata, head)
+            response = urllib.request.urlopen(req, timeout=2)
+            html = response.read().decode('utf-8')
+            target = json.loads(html)
+            #得到查询结果
+            langs = target['type'].split('2', 1)
+            trans = target['translateResult'][0][0]['tgt']
+            res = DataStructure(language_map[langs[0]], language_map[langs[1]], trans, trans_source='在线查询结果')
+            self.offline_data[content] = res
+        except (OSError,urllib.error.URLError,urllib.error.URLError):
+            res = None
+        finally:
+            return res
